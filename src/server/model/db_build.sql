cee1eda2c8453b21440d375b8c4d37b5726da30b
sallamtanna <sallamtanna2015@hotmail.com>
2018-08-01 09:53:15 +0300

    editing

--- src/server/model/db_build.sql
+++ src/server/model/db_build.sql
@@ -1,13 +1,15 @@
 BEGIN;
-DROP TABLE IF EXISTS users, items, orders;
+
+DROP TABLE IF EXISTS users, items, orders CASCADE;
 
 CREATE TABLE users(
 id SERIAL PRIMARY KEY,
 username VARCHAR(20) UNIQUE NOT NULL,
 email VARCHAR(100) UNIQUE NOT NULL,
+phone VARCHAR(100) UNIQUE NOT NULL,
 password VARCHAR(100),
 address TEXT,
-mPesaNumber VARCHAR
+m_pesa VARCHAR
 );
 
 CREATE TABLE items(
@@ -19,12 +21,12 @@ image VARCHAR
 
 CREATE TABLE orders(
 id SERIAL PRIMARY KEY,
-itemID INTEGER REFERENCES items(id),
-userID INTEGER REFERENCES users(id),
+item_id INTEGER REFERENCES items(id),
+user_id INTEGER REFERENCES users(id),
 quantity INTEGER,
 status VARCHAR,
-deliveryTime VARCHAR,
-trackingNumber VARCHAR
+delivery_time VARCHAR,
+traking_number VARCHAR
 );
 
 COMMIT;
