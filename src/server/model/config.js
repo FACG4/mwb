b38d4274075d1dd36ed142422accba95216417e8
IsraaMoqbel <isramm94@gmail.com>
2018-08-15 16:13:27 +0300

    adding console.log to debuge the error

--- src/server/model/config.js
+++ src/server/model/config.js
@@ -4,6 +4,7 @@ module.exports = () => {
   const {
     NODE_ENV, TEST_DB, PRODUCTION_DB, DB_URL,
   } = process.env;
+  console.log(NODE_ENV, TEST_DB, PRODUCTION_DB, DB_URL);
 
   switch (NODE_ENV) {
     case 'test': return TEST_DB;
