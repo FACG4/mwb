ee111678fd74b5d538dcee9800749d6bbb78b26c
IsraaMoqbel <isramm94@gmail.com>
2018-08-16 10:27:20 +0300

    adding error handling for unexpected error

--- src/server/controllers/tracker.js
+++ src/server/controllers/tracker.js
@@ -3,39 +3,45 @@ const update = require('../model/queries/update');
 const select = require('../model/queries/select');
 const { sendSMS } = require('./sendSMS');
 
+
 const tracker = (req, res) => {
-  const trakerNumber = req.body.trakerNumber;
-  const orderId = req.body.orderId;
+  try {
+    const trakerNumber = req.body.trakerNumber;
+    const orderId = req.body.orderId;
 
-  update.updateTrackerNumber(trakerNumber, orderId, (err, result) => {
-    if (err) {
-      return res.json({
-        status: false,
-        error: err,
-      });
-    }
-
-    select.selectUserBasedOnOrderId(result.rows[0].orderid, (err, result1) => {
+    update.updateTrackerNumber(trakerNumber, orderId, (err, result) => {
       if (err) {
         return res.json({
           status: false,
           error: err,
         });
       }
-      const targetPhone = result1.rows[0].phone;
 
-      sendSMS('+17192203059', `your tracking number is: ${trakerNumber}`, targetPhone, (err, done) => {
-        if (err) return res.json({ status: false, error: err });
+      select.selectUserBasedOnOrderId(result.rows[0].orderid, (err, result1) => {
+        if (err) {
+          return res.json({
+            status: false,
+            error: err,
+          });
+        }
+        const targetPhone = result1.rows[0].phone;
 
-        res.send({
-          status: true,
-          message: 'Success',
-          data: result,
-          smsResult: done,
+        sendSMS('+17192203059', `your tracking number is: ${trakerNumber}`, targetPhone, (err, done) => {
+          if (err) return res.json({ status: false, error: err });
+
+          res.send({
+            status: true,
+            message: 'Success',
+            data: result,
+            smsResult: done,
+          });
         });
       });
     });
-  });
+  } catch (err) {
+    console.log(err.stack);
+    res.json({ status: false, error: err, message: 'UNExpected' });
+  }
 };
 
 module.exports = {
